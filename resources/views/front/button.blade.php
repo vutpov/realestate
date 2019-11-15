eb556fa656d231c51e83bda2e83f88502e394c80
pov <vuthpov26@gmail.com>
2019-11-15 13:00:12 +0700

    change front home

--- resources/views/front/button.blade.php
+++
@@ -1,6 +0,0 @@
-<li class="list-inline-item">
-    <a target="_blank" href="/explore" class="btn btn-unique btn-lg btn-rounded mr-0">Start Exploring</a>
-</li>
-{{-- <li class="list-inline-item">
-    <a target="_blank" href="#" class="btn btn-primary btn-lg btn-rounded ml-0">Hi</a>
-</li> --}}
