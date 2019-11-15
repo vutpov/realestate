d2ddc3803459450181969f36c29bc5402317230a
pov <vuthpov26@gmail.com>
2019-11-15 13:42:24 +0700

    fix front

---
+++ resources/views/front/master.blade.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Real_state || Home</title>
+  <link rel="icon" href="{{url('img/favicon.png')}}">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="{{url('front/css/bootstrap.min.css')}}">
+  <!-- animate CSS -->
+  <link rel="stylesheet" href="{{url('front/css/animate.css')}}">
+  <!-- owl carousel CSS -->
+  <link rel="stylesheet" href="{{url('front/css/owl.carousel.min.css')}}">
+  <!-- themify CSS -->
+  <link rel="stylesheet" href="{{url('front/css/themify-icons.css')}}">
+  <!-- flaticon CSS -->
+  <link rel="stylesheet" href="{{url('front/css/flaticon.css')}}">
+  <!-- magnific-popup CSS -->
+  <link rel="stylesheet" href="{{url('front/css/magnific-popup.css')}}">
+  <!-- font awesome CSS -->
+  <link rel="stylesheet" href="{{url('front/fontawesome/css/all.min.css')}}">
+  <!-- style CSS -->
+  <link rel="stylesheet" href="{{url('front/css/style.css')}}">
+</head>
+
+<body>
+
+  @yield('content')
+
+  <!--::footer_part start::-->
+  <footer class="footer_part">
+      <div class="container">
+          <div class="row">
+              <div class="col-sm-6 col-lg-4">
+                  <div class="single_footer_part">
+                      <h4>About Us</h4>
+                      <p>Heaven fruitful doesn't over lesser days appear creeping seasons so behold bearing days open</p>
+                      <a href="index.html" class="footer_logo"><img src="{{url('front/img/full-logo.png')}}" class="img-fluid" alt="logo" width="300" height="100"> </a>
+                  </div>
+              </div>
+              <div class="col-sm-6 col-lg-4">
+                  <div class="single_footer_part">
+                      <h4>Contact Info</h4>
+                      <p>Address : # 65, St. 95, Sangkat Beoung Kengkarng, khan Chamkamon,
+                          Phnom Penh, Cambodia.
+                      </p>
+                      <p>Phone : (855) 23 978 999/ 78 338 000 </p>
+                      <p>Email : info@kniron.com </p>
+                      <p>Facebook : kniron</p>
+                  </div>
+              </div>
+              <div class="col-sm-6 col-lg-4">
+                  <div class="single_footer_part">
+                      <h4>Important Link</h4>
+                      <ul class="list-unstyled">
+                          <li><a href=""> WHMCS-bridge</a></li>
+                          <li><a href="">Search Domain</a></li>
+                          <li><a href="">My Account</a></li>
+                          <li><a href="">Shopping Cart</a></li>
+                          <li><a href="">Our Shop</a> Our Shop</li>
+                      </ul>
+                  </div>
+              </div>
+              <!-- <div class="col-sm-6 col-lg-3">
+                  <div class="single_footer_part">
+                      <h4>Newsletter</h4>
+                      <p>Heaven fruitful doesn't over lesser in days. Appear creeping seasons deve behold bearing days open
+                      </p>
+                      <div class="mail_part">
+                          <input type="text" placeholder="Email Address" class="placeholder hide-on-focus">
+                          <a href="" class="email_icon"><i class="far fa-paper-plane"></i></a>
+                      </div>
+                  </div>
+              </div> -->
+          </div>
+          <hr>
+          <div class="row">
+              <div class="col-sm-6 col-lg-6">
+                  <div class="copyright_text">
+                      <p>
+  
+                          Copyright ©<style>.fi-padding {padding-bottom: 0.2em;} @-webkit-keyframes fi-spin {from {-webkit-transform: rotate(0);-o-transform: rotate(0);transform: rotate(0);}to {-webkit-transform: rotate(360deg);-o-transform: rotate(360deg);transform: rotate(360deg);}}@keyframes fi-spin {from {-ms-transform: rotate(0);-moz-transform: rotate(0);-webkit-transform: rotate(0);-o-transform: rotate(0);transform: rotate(0);}to {-ms-transform: rotate(360deg);-moz-transform: rotate(360deg);-webkit-transform: rotate(360deg);-o-transform: rotate(360deg);transform: rotate(360deg);}}.fi-spin {-webkit-animation: fi-spin 2s linear infinite;-moz-animation: fi-spin 2s linear infinite;-ms-animation: fi-spin 2s linear infinite;-o-animation: fi-spin 2s linear infinite;animation: fi-spin 2s linear infinite;}.fi-spin-ccw {-webkit-animation: fi-spin 2s linear infinite;-moz-animation: fi-spin 2s linear infinite;-ms-animation: fi-spin 2s linear infinite;-o-animation: fi-spin 2s linear infinite;animation: fi-spin 2s linear infinite;animation-direction: reverse;}.fi-pulse {-webkit-animation: fi-spin 2s infinite steps(8);-moz-animation: fi-spin 2s infinite steps(8);-ms-animation: fi-spin 2s infinite steps(8);-o-animation: fi-spin 2s infinite steps(8);animation: fi-spin 2s infinite steps(8);}.fi-pulse-ccw {-webkit-animation: fi-spin 2s infinite steps(8);-moz-animation: fi-spin 2s infinite steps(8);-ms-animation: fi-spin 2s infinite steps(8);-o-animation: fi-spin 2s infinite steps(8);animation: fi-spin 2s infinite steps(8);animation-direction: reverse;}</style><script>
+                              document.write(new Date().getFullYear());
+                          </script>2019
+                          K Niron Real Estate
+                      </p>
+                  </div>
+              </div>
+              <div class="col-sm-6 col-lg-6">
+                  <div class="footer_icon">
+                      <ul class="list-unstyled">
+                          <li><a href="#" class="single_social_icon"><i class="fab fa-facebook-f"></i></a></li>
+                          <li><a href="#" class="single_social_icon"><i class="fab fa-twitter"></i></a></li>
+                          <li><a href="#" class="single_social_icon"><i class="fas fa-globe"></i></a></li>
+                          <li><a href="#" class="single_social_icon"><i class="fab fa-behance"></i></a></li>
+                      </ul>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
+  <!--::footer_part end::-->
+  <!-- jquery plugins here-->
+  <!-- jquery -->
+  <script src={{url("front/js/jquery-1.12.1.min.js")}}></script>
+  <!-- popper js -->
+  <script src={{url("front/js/popper.min.js")}}></script>
+  <!-- bootstrap js -->
+  <script src={{url("front/js/bootstrap.min.js")}}></script>
+  <!-- easing js -->
+  <script src={{url("front/js/jquery.magnific-popup.js")}}></script>
+  <!-- particles js -->
+  <script src={{url("front/js/owl.carousel.min.js")}}></script>
+  <!-- easing js -->
+  <script src={{url("front/js/jquery.easing.min.js")}}></script>
+  <!-- custom js -->
+  <script src={{url("front/js/custom.js")}}></script>
+</body>
+
+</html>
---
+++ resources/views/front/shared/home-nav.blade.php
@@ -0,0 +1,56 @@
+<header class="main_menu home_menu">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <a class="navbar-brand" href="index.php"><img src="{{url('front/img/full-logo.png')}}" class="img-fluid" alt="logo" width="300" height="100"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse main-menu-item" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+                            </li>
+
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    About
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="ceo_message.php">CEO Message </a>
+                                    <a class="dropdown-item" href="company_background.php">The Company Background</a>
+                                    <a class="dropdown-item" href="vision.php">Vision &amp; Mission</a>
+                                    <a class="dropdown-item" href="staff.php">Staff and Man Power</a>
+
+                                </div>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="product.php">Product &amp; Service</a>
+                            </li>
+                            <!-- <li class="nav-item">
+                                <a class="nav-link page-scroll" href="blog.php">Blog</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Pages
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="single-blog.php">Single blog</a>
+                                    <a class="dropdown-item" href="elements.php">Elements</a>
+                                </div>
+                            </li> -->
+                            <li class="nav-item">
+                                <a class="nav-link" href="contact.php">Contact us</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="btn_1 d-none d-lg-block">
+                        <a href="#" class="float-right">Submit property</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
